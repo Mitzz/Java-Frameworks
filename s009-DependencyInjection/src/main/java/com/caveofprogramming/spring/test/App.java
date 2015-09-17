e9ae1973db71d99bb1c485a5b883f23820eaa532
91014863 <91014863@192.168.2.7>
2015-09-17 17:27:31 +0530

    Stage 01 -Create Address class with two properties 'street' and 'postcode'. -Create Constructor for the same -Add 'address' bean in 'bean.xml' and use constructor-injection to populate values. -Test it in App.

---
+++ s009-DependencyInjection/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -0,0 +1,22 @@
+package com.caveofprogramming.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
+		
+		Person person = (Person) context.getBean("person");
+		Address address = (Address) context.getBean("address");
+		person.speak();
+		
+		System.out.println(address);
+		System.out.println(person);
+
+		((ClassPathXmlApplicationContext)context).close();
+		
+	}
+
+}
