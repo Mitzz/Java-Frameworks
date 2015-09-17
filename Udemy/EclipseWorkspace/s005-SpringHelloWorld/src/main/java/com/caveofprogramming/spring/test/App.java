d74addfbb68bdb247c49efffa8f6bc87419c6a46
91014863 <91014863@192.168.2.7>
2015-09-17 16:16:25 +0530

    Stage 02 Using spring to instantiate an object of Person.

--- Udemy/EclipseWorkspace/s005-SpringHelloWorld/src/main/java/com/caveofprogramming/spring/test/App.java
+++ Udemy/EclipseWorkspace/s005-SpringHelloWorld/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -1,10 +1,15 @@
 package com.caveofprogramming.spring.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 
 public class App {
 
 	public static void main(String[] args) {
-		Person person = new Person();
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		Person person = (Person) context.getBean("person");
 		person.speak();
 	}
 
