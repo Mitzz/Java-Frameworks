a2cc611e1d32eb5856969517d664311b0c9ed8ea
91014863 <91014863@192.168.2.7>
2015-09-20 13:23:38 +0530

    Stage 01 - Not using annotation for setting property value -beans.xml: Annotation-driven BD -Robot: Two property 'id' and 'speech'.One method 'speech()'. Property value not set by annotation but by default description given to instance variables

---
+++ Udemy/EclipseWorkspace/s032-SettingPropertyValueViaAnnotation/src/main/java/com/caveofprogramming/spring/test/Robot.java
@@ -0,0 +1,23 @@
+package com.caveofprogramming.spring.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Robot {
+
+	private int id = 0;
+	private String speech = "hello";
+	
+	public void speak(){
+		System.out.println(id + ":" + speech);
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setSpeech(String speech) {
+		this.speech = speech;
+	}
+	
+}
