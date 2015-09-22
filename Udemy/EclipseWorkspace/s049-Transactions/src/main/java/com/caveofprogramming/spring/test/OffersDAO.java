ffdf0656e4f8158c3b01f0999b6fc7c95a9b0399
91014863 <91014863@172.18.25.152>
2015-09-22 18:17:10 +0530

    Stage 03 - Using @Transaction -OffersDAO: Add @Transaction above 'int[] create(List<Offer> offers)' -App: Rerun Stage 01 and Stage 02. Only 2 rows are inserted instead of 3 rows.

--- Udemy/EclipseWorkspace/s049-Transactions/src/main/java/com/caveofprogramming/spring/test/OffersDAO.java
+++ Udemy/EclipseWorkspace/s049-Transactions/src/main/java/com/caveofprogramming/spring/test/OffersDAO.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class OffersDAO {
@@ -47,6 +48,7 @@ public class OffersDAO {
 		return jdbc.update("Insert into offers(name, text, email) values (:name, :text, :email)", params) == 1; 
 	}
 	
+	@Transactional
 	public int[] create(List<Offer> offers){
 		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
 		return jdbc.batchUpdate("Insert into offers(id, name, text, email) values (:id, :name, :text, :email)", params); 
