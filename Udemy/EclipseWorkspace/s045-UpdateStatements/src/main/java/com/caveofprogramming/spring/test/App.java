7a007660289dcdf6dd186d5eabf9bcfa240d889d
91014863 <91014863@192.168.2.7>
2015-09-22 06:24:03 +0530

    Stage 01 - Delete operation -OffersDAO: Add 'boolean deleteOffer(int id)' method -App: Test 'deleteOffer(int id)' with id '1'

---
+++ Udemy/EclipseWorkspace/s045-UpdateStatements/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -0,0 +1,42 @@
+package com.caveofprogramming.spring.test;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
+		System.out.println("------");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDAO");
+		
+		try{
+			offersDao.deleteOffer(1);
+			
+			List<Offer> offers = offersDao.getOffers();
+		
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}
+			
+			Offer offer = offersDao.getOffer(2);
+			System.out.println("Should be Mike: " + offer);
+			
+		}
+		catch(CannotGetJdbcConnectionException e){
+			System.out.println("Cannot get database connection");
+		}
+		catch(DataAccessException e){
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+		System.out.println("------");
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
---
+++ Udemy/EclipseWorkspace/s045-UpdateStatements/src/main/java/com/caveofprogramming/spring/test/OffersDAO.java
@@ -0,0 +1,70 @@
+package com.caveofprogramming.spring.test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OffersDAO {
+	
+	private NamedParameterJdbcTemplate jdbc;
+	
+	@Autowired
+	public void setDataSource(DataSource jdbc){
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+	
+	public List<Offer> getOffers() {
+		
+		return jdbc.query("Select * from offers", new RowMapper<Offer>(){
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setText(rs.getString("text"));
+				offer.setEmail(rs.getString("email"));
+				return offer;
+			}
+			
+		});
+		
+	}
+	
+	public Offer getOffer(int id) {
+		
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+		
+		return jdbc.queryForObject("Select * from offers where id = :id", params, new RowMapper<Offer>(){
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setText(rs.getString("text"));
+				offer.setEmail(rs.getString("email"));
+				return offer;
+			}
+			
+		});
+		
+	}
+	
+	public boolean deleteOffer(int id) {
+		
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+		
+		return jdbc.update("Delete from offers where id = :id", params) == 1;
+		
+	}
+}
