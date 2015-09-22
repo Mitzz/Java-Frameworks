c143e96ff015483b8d37b4c31f54d283e6c0e5f5
91014863 <91014863@172.18.25.152>
2015-09-22 20:26:51 +0530

    56 - Using Models Stage 04 -OffersController: Using 'Model' to set attribute

--- Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,24 +1,18 @@
 package com.caveofprogramming.spring.web.controllers;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OffersController {
 
 	@RequestMapping("/")
-	public ModelAndView showHome(HttpSession session){
-		ModelAndView mv = new ModelAndView("home");
+	public String showHome(Model model){
 		
-		Map<String, Object> model = mv.getModel();
-		model.put("name", "<b>River</b>");
+		model.addAttribute("name", "Tiffany");
 		
-		return mv;
+		return "home";
 		
 	}
 }
