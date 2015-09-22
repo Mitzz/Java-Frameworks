d33f4ba8d0514ec55e8e1f47afa8cd303fba53a9
91014863 <91014863@172.18.25.152>
2015-09-22 17:26:09 +0530

    Stage 01 - Batch Updates -OffersDAO: Add 'int[] create(List<Offer> offers)' method. Use 'SqlParameterSource' and 'SqlParameterSourceUtils' to create batch mapping. -App: Test 'create(List<Offer> offers)' method and check the no of rows affected for each Update Statement.

---
+++ Udemy/EclipseWorkspace/s048-PreparedStatementsBatchUpdate/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -0,0 +1,68 @@
+package com.caveofprogramming.spring.test;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
+		System.out.println("------");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDAO");
+		
+		try{
+			Offer offer1 = new Offer("Dave", "dave@caveofprogramming.com", "Coding Java");
+			Offer offer2 = new Offer("Karen", "karen@caveofprogramming.com", "Software Testing");
+			
+			if(offersDao.create(offer1)){
+				System.out.println("Offer object inserted");
+			}
+			if(offersDao.create(offer2)){
+				System.out.println("Offer object inserted");
+			}
+			
+			if(offersDao.deleteOffer(80)){
+				System.out.println("Offer object deleted");
+			}
+			
+			Offer updateOffer = new Offer(10, "Mitz", "mitz@caveofprogramming.com", "Software Architect");
+			
+			if(offersDao.update(updateOffer)){
+				System.out.println("Offer updated");
+			} else {
+				System.out.println("Cannot update offer");
+			}
+			
+			List<Offer> offers = offersDao.getOffers();
+			
+			int[] rvals = offersDao.create(offers);
+			
+			for (int value : rvals) {
+				System.out.println("Updated " + value + " rows.");
+			}
+		
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}
+			
+			Offer offer = offersDao.getOffer(2);
+			System.out.println("Should be Mike: " + offer);
+			
+		}
+		catch(CannotGetJdbcConnectionException e){
+			System.out.println("Cannot get database connection");
+		}
+		catch(DataAccessException e){
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+		System.out.println("------");
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
