92f67bd1701caf9f228942e4ac5ad746c16faf5f
91014863 <91014863@172.18.25.252>
2015-09-24 16:51:52 +0530

    64 - Getting URL parameters Stage 01 -OffersController: Add a request handler method 'String showTest(Model model, @RequestParam("id") String id)' for '/test'. It handles request of type 'GET' Test the code with /../test?id=435

--- Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.caveofprogramming.spring.web.dao.Offer;
 import com.caveofprogramming.spring.web.service.OffersService;
@@ -18,6 +20,12 @@ public class OffersController {
 	@Autowired
 	public void setOffersService(OffersService offersService) {
 		this.offersService = offersService;
+	}
+	
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id") String id) {
+		System.out.println("Id is: " + id);
+		return "home";
 	}
 
 	@RequestMapping("/offers")
