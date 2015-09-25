1038fdd740a2b0d3c3ce9c7bfb9093207c85de5e
91014863 <91014863@192.168.2.7>
2015-09-25 20:16:41 +0530

    69 - Form validations Stage 01 -OffersController.java: Modify method 'doCreate' by adding a parameter 'BindingResult result' to do form validation. Display all errors using 'ObjectError'. Use '@Valid Offer offer' to tell spring to validate form data. -pom.xml: Add Dependency. Search for 'java.validation' and Select 'javax.validation valid-api' v1.1.0 Final. Search for 'org.hibernate' and select 'org.hibernate hibernate' v3.5.4-Final. Search for 'hibernate-validator' and Select 'org.hibernate hibernate-validator' v5.0.1-Final

--- Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,10 +1,15 @@
 package com.caveofprogramming.spring.web.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,7 +48,20 @@ public class OffersController {
 	}
 	
 	@RequestMapping(value="/docreate", method=RequestMethod.POST)
-	public String doCreate(Model model, Offer offer){
+	public String doCreate(Model model,@Valid Offer offer, BindingResult result){
+		if(result.hasErrors()){
+			System.out.println("Form does not validate");
+			
+			List<ObjectError> errors = new ArrayList<ObjectError>();
+			
+			for(ObjectError error : errors){
+				System.out.println(error);
+			}
+			
+		} else {
+			System.out.println("Form validated");
+		}
+		
 		System.out.println(offer);
 		return "offercreated";
 	}
