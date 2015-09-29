7dabccf7dbabb2e4a72b6866cd616a0907ad1b3e
91014863 <91014863@172.18.25.152>
2015-09-29 19:39:27 +0530

    75 - Handling Exception Stage 02 - Exception Handler -OffersController.java: Add a method 'String handleDatabaseException(DataAccessException ex)'. Add '@ExceptionHandler(DataAccessException.class)' -error.jsp: A new jsp in '/WEB-INF/jsps/'

--- Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +33,11 @@ public class OffersController {
 	public String showTest(Model model, @RequestParam("id") String id) {
 		System.out.println("Id is: " + id);
 		return "home";
+	}
+	
+	@ExceptionHandler(DataAccessException.class)
+	public String handleDatabaseException(DataAccessException ex){
+		return "error";
 	}
 
 	@RequestMapping("/offers")
