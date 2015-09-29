46236ac278062d389d729662d620350412b18cdd
91014863 <91014863@172.18.25.152>
2015-09-29 17:55:15 +0530

    86 - Args Pointcut Designator Stage 09 - 'Double' type: No Casting -Logger: Modify pointcut with id 'somePointcut' as @Pointcut("args(Double)")

--- Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
+++ Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-	@Pointcut("args(Object)")
+	@Pointcut("args(Double)")
 	public void somePointcut(){}
 	
 	@Before("somePointcut()")
