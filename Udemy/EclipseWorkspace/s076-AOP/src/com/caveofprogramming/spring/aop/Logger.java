54e9a6998c36e8709de6fe49dafe4892326f2168
91014863 <91014863@172.18.25.152>
2015-09-29 17:28:40 +0530

    85 - Bean Pointcut Designator Stage 01 -Logger: Modify pointcut with id 'somePointcut' as @Pointcut("bean(camera)")

--- Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
+++ Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-	@Pointcut("@args(java.lang.Deprecated)")
+	@Pointcut("bean(camera)")
 	public void somePointcut(){}
 	
 	@Before("somePointcut()")
