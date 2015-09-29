236cd43677c88d6d2ab13db295b460d6f5938056
91014863 <91014863@172.18.25.152>
2015-09-29 17:52:24 +0530

    86 - Args Pointcut Designator Stage 07 - 'args' pcd example: Casting type -Logger: Modify pointcut with id 'somePointcut' as @Pointcut("args(double)")

--- Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
+++ Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-	@Pointcut("args(.., double)")
+	@Pointcut("args(double)")
 	public void somePointcut(){}
 	
 	@Before("somePointcut()")
