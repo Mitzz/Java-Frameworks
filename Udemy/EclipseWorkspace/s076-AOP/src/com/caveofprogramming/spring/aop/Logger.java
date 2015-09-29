532d769464add0dea727e669af129e8ae818b84a
91014863 <91014863@172.18.25.152>
2015-09-29 18:24:32 +0530

    88 - Getting Target Method Argument using 'args' Stage 03 - Pointcut with arguments with elipses -Logger: Modify pointcut with id 'somePointcut'. Modify advice to '@Before("somePointcut(exposure)")' and also the method code

--- Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
+++ Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-	@Pointcut("args(exposure, aperture)")
-	public void somePointcut(int exposure, double aperture){}
+	@Pointcut("args(exposure, ..)")
+	public void somePointcut(int exposure){}
 	
-	@Before("somePointcut(exposure, aperture)")
-	public void somePointcutDemo(JoinPoint jp, int exposure, double aperture) {
+	@Before("somePointcut(exposure)")
+	public void somePointcutDemo(JoinPoint jp, int exposure) {
 		System.out.println("********* BEFORE DEMO **********");
 		
-		System.out.printf("exposure %d, aperture %.2f\n", exposure, aperture);
+		System.out.printf("exposure %d\n", exposure);
 	}
 	
 }
