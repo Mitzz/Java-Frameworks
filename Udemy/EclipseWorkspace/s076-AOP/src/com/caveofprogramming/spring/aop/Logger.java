48d1aed2b4c566b25e03ffd321be7711b590cbc4
91014863 <91014863@172.18.25.152>
2015-09-29 18:27:42 +0530

    88 - Getting Target Method Argument using 'args' Stage 04 - Removing 'JointPoint' argument -Logger: Removing 'JointPoint' argument in advice '@Before("somePointcut(exposure, aperture)")'

--- Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
+++ Udemy/EclipseWorkspace/s076-AOP/src/com/caveofprogramming/spring/aop/Logger.java
@@ -1,6 +1,5 @@
 package com.caveofprogramming.spring.aop;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -10,14 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-	@Pointcut("args(exposure, ..)")
-	public void somePointcut(int exposure){}
+	@Pointcut("args(exposure, aperture)")
+	public void somePointcut(int exposure, double aperture){}
 	
-	@Before("somePointcut(exposure)")
-	public void somePointcutDemo(JoinPoint jp, int exposure) {
+	@Before("somePointcut(exposure, aperture)")
+	public void somePointcutDemo(int exposure, double aperture) {
 		System.out.println("********* BEFORE DEMO **********");
 		
-		System.out.printf("exposure %d\n", exposure);
+		System.out.printf("exposure %d, aperture %.2f\n", exposure, aperture);
 	}
 	
 }
