6707f00b8d431c8860d545fb7fe7a1f6d89f7b57
91014863 <91014863@172.18.25.152>
2015-09-30 19:04:32 +0530

    98 - Adding New Account Form Stage 01 -newaccount.jsp: A new file in folder 'WEB-INF/jsps/'. Code copied from createoffer.jsp and modified -accountcreated.jsp: A new file in folder 'WEB-INF/jsps/'. Simple jsp -OffersController.java: Add a request handler 'showNewAccount' for request '/newaccount' and redirect it to 'newaccount'. Add a request handler 'createAccount' for request '/createaccount' and redirect it to 'accountcreated' -User.java: A new model in a package 'com.caveofprogramming.spring.web.dao'. Add necessary properties and constructors -security-context.xml: Add 'intercept-url' using 'Source' view with pattern '/newaccount', '/createaccount' and '/accountcreated' with access 'permitAll' -login.jsp: Link to create a new account using 'c:url'

--- Udemy/EclipseWorkspace/s051-BringingInMaven/WebContent/WEB-INF/jsps/login.jsp
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/WebContent/WEB-INF/jsps/login.jsp
@@ -30,5 +30,7 @@
 			</tr>
 		</table>
 	</form>
+	
+	<p><a href="<c:url value="/newaccount"/>">Create new account</a></p>
 </body>
 </html>
--- Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/LoginController.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.caveofprogramming.spring.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.caveofprogramming.spring.web.dao.User;
 
 @Controller
 public class LoginController {
@@ -9,5 +12,17 @@ public class LoginController {
 	@RequestMapping("/login")
 	public String showLogin(){
 		return "login";
+	}
+	
+	@RequestMapping("/newaccount")
+	public String showNewAccount(Model model) {
+		
+		model.addAttribute("user", new User());
+		return "newaccount";
+	}
+	
+	@RequestMapping("/createaccount")
+	public String createAccount() {
+		return "accountcreated";
 	}
 }
