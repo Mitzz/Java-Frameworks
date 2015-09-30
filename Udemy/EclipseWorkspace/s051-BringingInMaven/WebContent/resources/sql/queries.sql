737f7a962ef3a075e1d0198a982977cf38a9a466
91014863 <91014863@172.18.25.152>
2015-09-30 18:08:20 +0530

    97 - Authorising Users via Database Stage 01 -main.css: Modify a selector -security-context.xml: RC 'authentication-manager' and select 'authentication-provider'. RC 'authentication-provider' and select 'jdbc-user-service'. Select 'jdbc-user-service', give 'data-source-ref' as 'dataSource'(bean id from dao-context.xml) -queries.sql: A new file in new folder 'WebContent/resources/sql/'. Creating table 'users' and 'authorities'

---
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/WebContent/resources/sql/queries.sql
@@ -0,0 +1,15 @@
+create table users(
+	username varchar(60) not null primary key,
+	password varchar(80),
+	enabled tinyint default 1
+);
+
+create table authorities(
+	username varchar(60) primary key,
+	authority varchar(45)
+);
+
+insert into users(username, password) values ("Mike", "hello");
+select * from users;
+insert into authorities(username, authority ) values ("Mike", "admin");
+select * from authorities;
