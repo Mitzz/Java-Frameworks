30296a8385a84e5e83e14247be2a5aebfc9bc6a6
91014863 <91014863@91014863_WWIT>
2015-10-03 14:46:41 +0530

    125 - JUnit Dao Tests Stage 01 -UserDaoTests.java: Add instance variable 'usersDao' of type 'UsersDao' and 'dataSource' of type 'DataSource' with @Autowired. Add method 'init' with @Before RC on 'UserDaoTests', select 'Run As' and click 'JUnit Test'

--- Udemy/EclipseWorkspace/s051-BringingInMaven/test/com/caveofprogramming/spring/web/test/tests/UserDaoTests.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/test/com/caveofprogramming/spring/web/test/tests/UserDaoTests.java
@@ -2,11 +2,18 @@ package com.caveofprogramming.spring.web.test.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.sql.DataSource;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.caveofprogramming.spring.web.dao.UsersDao;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations = {
@@ -15,6 +22,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
+	
+	@Autowired
+	private UsersDao usersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Before
+	public void init(){
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+	}
 
 	@Test
 	public void testCreateUser(){
