2223b8f8377ec3dc5fb778b599a68884e76b9780
91014863 <91014863@192.168.2.7>
2015-10-04 20:17:04 +0530

    132 - Changing Jsp Links based on Database Stage 01 -HomeController.java: Add instance variable 'offersService' of type 'OffersService'. Modify code in method 'showHome'(Code copied from OffersController method 'showOffers') -home.jsp: Modify change (Code copied from offers.jsp)

--- Udemy/EclipseWorkspace/s051-BringingInMaven/WebContent/WEB-INF/tiles/home.jsp
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/WebContent/WEB-INF/tiles/home.jsp
@@ -4,6 +4,24 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="sec"
 	uri="http://www.springframework.org/security/tags"%>
+	
+	
+<table class="offers">
+<tr><td>Name</td><td>Email</td><td>Offer</td></tr>
+
+<c:forEach var="offer" items="${offers}">
+<tr>
+
+<td><c:out value="${offer.user.name}"></c:out></td>
+
+<td><c:out value="${offer.user.email}"></c:out></td>
+
+<td><c:out value="${offer.text}"></c:out></td>
+   
+</tr>
+</c:forEach>
+</table>
+
 <p><a href="${pageContext.request.contextPath}/offers">Show Current Offers</a></p>
 <p><a href="${pageContext.request.contextPath}/createoffer">Add a new offer</a></p>
 
--- Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/HomeController.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/src/com/caveofprogramming/spring/web/controllers/HomeController.java
@@ -1,18 +1,30 @@
 package com.caveofprogramming.spring.web.controllers;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.service.OffersService;
 
 @Controller
 public class HomeController {
 	
 	private static Logger logger = Logger.getLogger(HomeController.class);
+	
+	@Autowired
+	private OffersService offersService;
 
 	@RequestMapping("/")
-	public String showHome() {
+	public String showHome(Model model, Principal principal) {
 		
-		logger.info("Showing home page...");
+		List<Offer> offers = offersService.getCurrent();
+		model.addAttribute("offers", offers);
 		return "home";
 	}
 	
