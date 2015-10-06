0cfc0d5f7b9b722c515a43595738b0d672b39703
91014863 <91014863@172.18.25.152>
2015-10-06 19:47:22 +0530

    144 - Queries with Multiple Criteria Stage 02 - Test Refractor -OfferDaoTests.java: Refractor

--- Udemy/EclipseWorkspace/s051-BringingInMaven/test/com/caveofprogramming/spring/web/test/tests/OfferDaoTests.java
+++ Udemy/EclipseWorkspace/s051-BringingInMaven/test/com/caveofprogramming/spring/web/test/tests/OfferDaoTests.java
@@ -28,8 +28,6 @@ import com.caveofprogramming.spring.web.dao.UsersDao;
 		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class OfferDaoTests {
-	
-
 
 	@Autowired
 	private OffersDao offersDao;
@@ -39,24 +37,25 @@ public class OfferDaoTests {
 
 	@Autowired
 	private DataSource dataSource;
-	
+
 	private User user1 = new User("johnwpurcell", "John Purcell", "hellothere",
 			"john@caveofprogramming.com", true, "ROLE_USER");
-	private User user2 = new User("richardhannay", "Richard Hannay", "the39steps",
-			"richard@caveofprogramming.com", true, "ROLE_ADMIN");
-	private User user3 = new User("suetheviolinist", "Sue Black", "iloveviolins",
-			"sue@caveofprogramming.com", true, "ROLE_USER");
+	private User user2 = new User("richardhannay", "Richard Hannay",
+			"the39steps", "richard@caveofprogramming.com", true, "ROLE_ADMIN");
+	private User user3 = new User("suetheviolinist", "Sue Black",
+			"iloveviolins", "sue@caveofprogramming.com", true, "ROLE_USER");
 	private User user4 = new User("rogerblake", "Rog Blake", "liberator",
 			"rog@caveofprogramming.com", false, "user");
-
 
 	private Offer offer1 = new Offer(user1, "This is a test offer.");
 	private Offer offer2 = new Offer(user1, "This is another test offer.");
 	private Offer offer3 = new Offer(user2, "This is yet another test offer.");
 	private Offer offer4 = new Offer(user3, "This is a test offer once again.");
-	private Offer offer5 = new Offer(user3, "Here is an interesting offer of some kind.");
+	private Offer offer5 = new Offer(user3,
+			"Here is an interesting offer of some kind.");
 	private Offer offer6 = new Offer(user3, "This is just a test offer.");
-	private Offer offer7 = new Offer(user4, "This is a test offer for a user that is not enabled.");
+	private Offer offer7 = new Offer(user4,
+			"This is a test offer for a user that is not enabled.");
 
 	@Before
 	public void init() {
@@ -65,30 +64,58 @@ public class OfferDaoTests {
 		jdbc.execute("delete from offers");
 		jdbc.execute("delete from users");
 	}
-	
+
 	@Test
 	public void testCreateRetrieve() {
 		usersDao.create(user1);
 		usersDao.create(user2);
 		usersDao.create(user3);
 		usersDao.create(user4);
-		
+
 		offersDao.create(offer1);
-		
+
 		List<Offer> offers1 = offersDao.getOffers();
 		assertEquals("Should be one offer.", 1, offers1.size());
-		
-		assertEquals("Retrieved offer should equal inserted offer.", offer1, offers1.get(0));
-		
+
+		assertEquals("Retrieved offer should equal inserted offer.", offer1,
+				offers1.get(0));
+
 		offersDao.create(offer2);
 		offersDao.create(offer3);
 		offersDao.create(offer4);
 		offersDao.create(offer5);
 		offersDao.create(offer6);
 		offersDao.create(offer7);
-		
+
 		List<Offer> offers2 = offersDao.getOffers();
-		assertEquals("Should be six offers for enabled users.", 6, offers2.size());
+		assertEquals("Should be six offers for enabled users.", 6,
+				offers2.size());
+
+	}
+
+	@Test
+	public void testGetUsername() {
+		usersDao.create(user1);
+		usersDao.create(user2);
+		usersDao.create(user3);
+		usersDao.create(user4);
+
+		offersDao.create(offer1);
+		offersDao.create(offer2);
+		offersDao.create(offer3);
+		offersDao.create(offer4);
+		offersDao.create(offer5);
+		offersDao.create(offer6);
+		offersDao.create(offer7);
+
+		List<Offer> offers1 = offersDao.getOffers(user3.getUsername());
+		assertEquals("Should be three offers for this user.", 3, offers1.size());
+
+		List<Offer> offers2 = offersDao.getOffers("sdfsfd");
+		assertEquals("Should be zero offers for this user.", 0, offers2.size());
+
+		List<Offer> offers3 = offersDao.getOffers(user2.getUsername());
+		assertEquals("Should be 1 offer for this user.", 1, offers3.size());
 	}
 
 	@Test
@@ -125,24 +152,26 @@ public class OfferDaoTests {
 		Offer offer2 = new Offer(user, "This is a test offer.");
 
 		offersDao.create(offer2);
-		
+
 		List<Offer> userOffers = offersDao.getOffers(user.getUsername());
 		assertEquals("Should be two offers for user.", 2, userOffers.size());
-		
+
 		List<Offer> secondList = offersDao.getOffers();
-		
-		for(Offer current: secondList) {
+
+		for (Offer current : secondList) {
 			Offer retrieved = offersDao.getOffer(current.getId());
-			
-			assertEquals("Offer by ID should match offer from list.", current, retrieved);
+
+			assertEquals("Offer by ID should match offer from list.", current,
+					retrieved);
 		}
-		
+
 		// Test deletion
 		offersDao.delete(offer.getId());
 
 		List<Offer> finalList = offersDao.getOffers();
 
-		assertEquals("Offers lists should contain one offer.", 1, finalList.size());
+		assertEquals("Offers lists should contain one offer.", 1,
+				finalList.size());
 	}
 
 }
